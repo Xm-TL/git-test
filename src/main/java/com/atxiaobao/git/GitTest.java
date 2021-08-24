2b0b42b2877f7e509d16ffc535ce218663eb41af
Xm <Xm@qq.com>
2021-08-24 10:36:34 +0800

    first commit

---
+++ src/main/java/com/atxiaobao/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atxiaobao.git;
+
+/**
+ * @author xm
+ * @create 2021-08-24 10:31
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello!git!");
+    }
+}
